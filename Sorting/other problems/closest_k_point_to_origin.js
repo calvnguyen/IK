e30690f9db6cfa0d324acb3fe0750ac09b96a07c
Calvin <imcalv@gmail.com>
2020-12-04 00:37:49 -0800

    closest pt to origin

---
+++ Sorting/other problems/closest_k_point_to_origin.js
@@ -0,0 +1,72 @@
+/* create a max heap on distance
+   add each point to the heap
+   while adding each point, if heap.size > K, then pop the top off the heap (top is the current furthest point)
+   return whatever is left in the heap
+*/ 
+
+var kClosest = function(points, K) {
+    let out = [];
+    function dist(point) {
+        return (point[0] * point[0]) + (point[1] * point[1]);
+    }
+    
+    let heap = new Heap((a, b) => dist(b) - dist(a));
+    
+    for(let point of points) {
+        heap.add(point);
+        if (heap.size > K) {
+            heap.poll();
+        }
+    }
+    return heap.data;
+};
+
+class Heap {
+    constructor(comparator) {
+        this.data = [];
+        this.comparator = comparator || ((parent, child) => parent - child);
+    }
+
+    get size() {
+        return this.data.length;
+    }
+
+    bubbleUp(c) {
+        if (c === 0) return;
+        const p = Math.floor((c + 1) / 2) - 1;
+        if (this.comparator(this.data[p], this.data[c]) > 0) {
+            [this.data[p], this.data[c]] = [this.data[c], this.data[p]];
+        }
+        this.bubbleUp(p);
+    }
+
+    bubbleDown(p) {
+        const c = 2 * (p + 1) - 1;
+        if (c >= this.data.length) return;
+
+        const leftDelta = this.comparator(this.data[p], this.data[c]);
+        const rightDelta = c + 1 >= this.data.length ? 0 : this.comparator(this.data[p], this.data[c + 1]);
+        if (leftDelta <= 0 && rightDelta <= 0) return;
+
+        const swapChildIndex = c + (leftDelta <= rightDelta);
+        [this.data[p], this.data[swapChildIndex]] = [this.data[swapChildIndex], this.data[p]];
+        this.bubbleDown(swapChildIndex);
+    }
+
+    add(val) {
+        this.data.push(val);
+        this.bubbleUp(this.data.length - 1);
+    }
+
+    poll() {
+        if (this.size < 2) return this.data.pop();
+        [this.data[0], this.data[this.size - 1]] = [this.data[this.size - 1], this.data[0]];
+        const val = this.data.pop();
+        this.bubbleDown(0);
+        return val;
+    }
+
+    peek() {
+        return this.data[0];
+    }
+}
