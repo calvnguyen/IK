190655e20b5a3036406658a2c537351d87e47e10
Calvin <imcalv@gmail.com>
2020-12-05 17:22:06 -0800

    palindrome prob

---
+++ Recursion/Other problems/isPalindrome.js
@@ -0,0 +1,26 @@
+
+
+// A Palindrome is a string / word that reads the same forward and backward
+
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true;
+  }
+
+  let firstLetter = string[0];
+  let lastLetter = string[string.length - 1];
+
+  if (firstLetter === lastLetter) {
+    let strWithoutFirstLastChars = string.substring(
+      1,
+      string.length - 1
+    );
+    // check towards inside from left and right
+    return isPalindrome(strWithoutFirstLastChars);
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('racescar'));
