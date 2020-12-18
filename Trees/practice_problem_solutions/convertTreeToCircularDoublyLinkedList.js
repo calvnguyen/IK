dc28e427bfdcd8ecade5d09ee36b4324c38036ec
Calvin <imcalv@gmail.com>
2020-12-17 23:33:40 -0800

    tree to doubly linkedlist solution

--- Trees/practice_problem_solutions/convertTreeToCircularDoublyLinkedList.js
+++ Trees/practice_problem_solutions/convertTreeToCircularDoublyLinkedList.js
@@ -11,6 +11,7 @@ class TreeNode {
 *  S(N): O(N)
 */
 // store the head (first) and tail (last) of the doubly linked list
+
 class FirstLast {
     constructor() {
         this.first = new TreeNode(null);
@@ -18,52 +19,61 @@ class FirstLast {
     }
 }
 
+let flLeft = new FirstLast();
+let flRight = new FirstLast();
+
 function BTtoLL(root) 
 {
     if (!root) {
-        return null;
+        return root;
     }
 
     let firstLast = new FirstLast();
 
-    dfs(root, firstLast);
+    firstLast = dfs(root);
 
     // set the circular links
-    firstLast.first.left_ptr = firstLast.last;
     firstLast.last.right_ptr = firstLast.first;
-
+    firstLast.first.left_ptr = firstLast.last;
+    
     return firstLast.first;
 
 };
 
 /* In-order traversal solution.
 */
-function dfs(root, firstLast)
-{
-  if (!root) return;
+function dfs(root) {
+    // base case - leaf node
+    let firstLast = new FirstLast();
+    if (isLeaf(root)) {
+        firstLast.first = root;
+        firstLast.last = root;
+        return firstLast;
+    }
 
-  if (root.left_ptr) {
-    dfs(root.left_ptr, firstLast);
-  }
+    if (root.left_ptr) {
+        flLeft = dfs(root.left_ptr);
+        firstLast.first = flLeft.first;
+        flLeft.last.right_ptr = root;
+        root.left_ptr = flLeft.last
+    } else {
+        firstLast.first = root;
+    }
 
-  // current Node, set first node to root if it doesn't exist
-  if (firstLast.first.val === null) {
-    firstLast.first = root;
-  }
+    if (root.right_ptr) {
+        flRight = dfs(root.right_ptr);
+        firstLast.last = flRight.last;
+        flRight.first.left_ptr = root;
+        root.right_ptr = flRight.first;
+    } else {
+        firstLast.last = root;
+    }
 
-  let prev = firstLast.last;
+    return firstLast;
+}
 
-  if (prev) {
-    prev.right_ptr = root;  // next
-    root.left_ptr = prev;   // previous
-  }
-
-  // last node
-  firstLast.last = root;
-
-  if (root.right_ptr) {
-    dfs(root.right_pt, firstLast);
-  }
+function isLeaf(node) {
+    return !node.left_ptr && !node.right_ptr;
 }
 
 const root = new TreeNode(10);
@@ -72,4 +82,4 @@ root.right_ptr = new TreeNode(30);
 root.left_ptr.left_ptr = new TreeNode(40);
 root.left_ptr.right_ptr = new TreeNode(50);
 
-BTtoLL(root);
+console.log(BTtoLL(root));
